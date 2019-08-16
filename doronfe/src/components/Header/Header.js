c2f9a7b296f6eaade539a7b82c6cc1b0ea9bd010
A10ND <alon121211@gmail.com>
2019-08-16 18:38:16 +0300

    Added Header support for mobile

--- doronfe/src/components/Header/Header.js
+++ doronfe/src/components/Header/Header.js
@@ -1,32 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
-import logo from '../../assets/doronLogo.png';
-
+import logo from "../../assets/doronLogo.png";
 
 function Header() {
+  const [burgerStyle, setBurgerStyle] = useState("");
+  function handleClickOnBurger() {
+    if (burgerStyle === "") {
+      setBurgerStyle("is-active");
+    } else {
+      setBurgerStyle("");
+    }
+  }
   return (
     <header>
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-          <a className="navbar-item" href="http://localhost:3000">
-            <img src={logo} alt="doronLogo" width="130" height="65" className="headerLogo" />
-          </a>
-          <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="headerNavbarLinks">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
+          <NavLink className="navbar-item" to="/">
+            <img
+              src={logo}
+              alt="doronLogo"
+              width="130"
+              height="65"
+              className="headerLogo"
+            />
+          </NavLink>
+          <a
+            role="button"
+            className="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="headerNavbarLinks"
+            onClick={handleClickOnBurger}
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
           </a>
         </div>
-        <div id="headerNavbarLinks" className="navbar-menu">
+        <div id="headerNavbarLinks" className={"navbar-menu " + burgerStyle}>
           <div className="navbar-end">
-            <NavLink className="navbar-item" to="/" exact>דף הבית</NavLink>
-            <NavLink className="navbar-item" to="/about">אודותינו</NavLink>
-            <NavLink className="navbar-item" to="/contactus">צור קשר</NavLink>
+            <NavLink
+              className="navbar-item"
+              to="/"
+              onClick={handleClickOnBurger}
+              exact
+            >
+              דף הבית
+            </NavLink>
+            <NavLink
+              className="navbar-item"
+              to="/about"
+              onClick={handleClickOnBurger}
+            >
+              אודותינו
+            </NavLink>
+            <NavLink
+              className="navbar-item"
+              to="/contactus"
+              onClick={handleClickOnBurger}
+            >
+              צור קשר
+            </NavLink>
           </div>
         </div>
       </nav>
-    </header >
+    </header>
   );
 }
 
