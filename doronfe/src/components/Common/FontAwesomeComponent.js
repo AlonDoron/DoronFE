9167213cb68071afb3332819ac5d14761e4f371a
AlonDoron <54036339+AlonDoron@users.noreply.github.com>
2019-08-19 22:42:44 +0300

    Create FontAwesomeComponent.js

---
+++ doronfe/src/components/Common/FontAwesomeComponent.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function FontAwesomeComponent(props) {
+  return (
+    <span className="icon is-right">
+      <FontAwesomeIcon icon={props.faIcon} />
+    </span>
+  );
+}
+
+export default FontAwesomeComponent;
