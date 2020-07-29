f602b68d78809d59353cc9d0419cf852c45652a2
AlonDoron <alon1212112@gmail.com>
2020-07-29 14:05:51 +0300

    Updated status codes and server route api

--- doronfe/src/config/server-config.js
+++ doronfe/src/config/server-config.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = "http://localhost:3000/api";
+export const SERVER_URL = "http://localhost:3001";
 export const HEADERS = {
-  "Content-Type": "application/json"
+  "Content-Type": "application/json",
 };
